4b006df1696351fc7662010c2ed95cf05fb35d76
RockSolidHead <38844691+RockSolidHead@users.noreply.github.com>
2018-05-29 23:38:16 +0900

    Add files via upload

---
+++ Luminous-Test.ino
@@ -0,0 +1,236 @@
+/*-------------------------------------------------
+  I2C経由で状態表示用LEDシステムを制御するプログラム
+  by RockSolidHead
+*-------------------------------------------------*/
+#include  <Wire.h>
+#include  "LuminousCommon.h"
+
+byte  i2cBuffer[33];
+
+void setup() {
+  Serial.begin(19200);
+  Wire.begin(); // I2c Start,Master
+  Serial.println("Luminous Test program.");
+  Serial.print("Waiting");
+  while( !IsWakeUpLUMI() ) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Luminous Wake UP!");
+  randomSeed(100);
+}
+#define WAIT  600
+void loop() {
+  int i,j;
+  int c1,c2;
+
+  Serial.println("---- BLINK TEST ----");
+
+  Serial.println("フルカラーLED 各LEDを個別に設定");
+  for( i=0 ; i<LED_FULL_COLOR_NUM ; i++ ) {
+    c1=random(0,14);
+    do { 
+      c2=random(0,14);
+      if( c2!=c1 )  break; 
+    }while(1);
+    setFCLmode((byte)(0xff & i ), LED_BLINK|LED_SPEED_LEVEL3, (byte)(0xff & c1),(byte)(0xff & c2) );
+  }
+  delay(5000);
+  
+  Serial.println("フルカラーLED 全てのLEDを一括でサクラ色で点滅");
+  setAllFCLmode(LED_BLINK| LED_SPEED_LEVEL5,LED_SAKURA,LED_BLACK);
+  delay(5000);
+
+  Serial.println("フルカラーLED 全体の明るさを制御");
+  setAllFCLmode(LED_ON,LED_WHITE,LED_BLACK);
+  Serial.println(" 明るく");
+  for( i=0 ; i< 5 ; i++ ) {
+    setFCLbright( 1 );
+    delay(100);
+  }
+  Serial.println(" 暗く");
+  for( i=0 ; i< 5 ; i++ ) {
+    setFCLbright( 2 );
+    delay(100);
+  }
+
+  setAllFCLmode(LED_OFF,LED_BLACK,LED_BLACK);
+  Serial.println("モノカラーLEDを一括で同じ点滅設定");
+  i2cBuffer[0]=LUM_MCL_ALL_SET;
+  for( i=0; i<LED_MONO_COLOR_NUM ; i++ ) {
+    i2cBuffer[1+i]= LED_BLINK|LED_SPEED_LEVEL6;
+  }
+  sendMCLmodeAll();
+  delay(10000);
+
+  Serial.println("モノカラーLED 長く消灯１回点灯");
+  i2cBuffer[0]=LUM_MCL_ALL_SET;
+  for( i=0; i<LED_MONO_COLOR_NUM ; i++ ) {
+    i2cBuffer[1+i]= LED_BLINK_OFFON|LED_SPEED_LEVEL6;
+  }
+  sendMCLmodeAll();
+  delay(10000);
+
+  Serial.println("モノカラーLED 長く消灯２回点灯");
+  i2cBuffer[0]=LUM_MCL_ALL_SET;
+  for( i=0; i<LED_MONO_COLOR_NUM ; i++ ) {
+    i2cBuffer[1+i]= LED_BLINK_OFFONON|LED_SPEED_LEVEL6;
+  }
+  sendMCLmodeAll();
+  delay(10000);
+
+  Serial.println("モノカラーLED　だんだん明るくだんだん暗く");
+  for( i=0; i<LED_MONO_COLOR_NUM ; i++ ) {
+    if( i<2 ) {//ON/OFFしかできなLEDは点滅設定
+      i2cBuffer[1+i]= LED_OFF;
+    } else {
+      i2cBuffer[1+i]= LED_BLINK_STUD|LED_SPEED_LEVEL13;
+    }
+  }
+  sendMCLmodeAll();
+  delay(10000);
+
+  Serial.println("モノカラーLED　急に明る急に暗く");
+  for( i=0; i<LED_MONO_COLOR_NUM ; i++ ) {
+    if( i<2 ) {//ON/OFFしかできなLEDは点滅設定
+      i2cBuffer[1+i]= LED_OFF;
+    } else {
+      i2cBuffer[1+i]= LED_BLINK_ST2UD|LED_SPEED_LEVEL13;
+    }
+  }
+  sendMCLmodeAll();
+  delay(10000);
+
+  Serial.println("モノカラーLED　だんだん明るく消灯");
+  for( i=0; i<LED_MONO_COLOR_NUM ; i++ ) {
+    if( i<2 ) {//ON/OFFしかできなLEDは点滅設定
+      i2cBuffer[1+i]= LED_OFF;
+    } else {
+      i2cBuffer[1+i]= LED_BLINK_STU|LED_SPEED_LEVEL13;
+    }
+  }
+  sendMCLmodeAll();
+  delay(10000);
+
+  Serial.println("フルカラーLED 任意のRGBカラーを設定");
+  Serial.println("だんだん明るくだんだん暗く");
+  for( j=0;j<LED_FULL_COLOR_NUM;j++ ) {
+    setFCLmode((byte)(0xff & j ), LED_BLINK_STUD|LED_SPEED_LEVEL13, LED_BLACK,LED_BLACK );   
+    setFCLcolor( j , random(0,256), random(0,256), random(0,256),FRONT );
+    setFCLcolor( j , random(0,256), random(0,256), random(0,256),BACK );
+  }
+  delay(5000);
+  Serial.println("急に明る急に暗く");
+  for( j=0;j<LED_FULL_COLOR_NUM;j++ ) {
+    setFCLmode((byte)(0xff & j ), LED_BLINK_ST2UD|LED_SPEED_LEVEL13, LED_BLACK,LED_BLACK );   
+    setFCLcolor( j , random(0,256), random(0,256), random(0,256),FRONT );
+    setFCLcolor( j , random(0,256), random(0,256), random(0,256),BACK );
+  }
+  delay(5000);
+  Serial.println("だんだん明るく消灯");
+  for( j=0;j<LED_FULL_COLOR_NUM;j++ ) {
+    setFCLmode((byte)(0xff & j ), LED_BLINK_STU|LED_SPEED_LEVEL13, LED_BLACK,LED_BLACK );   
+    setFCLcolor( j , random(0,256), random(0,256), random(0,256),FRONT );
+    setFCLcolor( j , random(0,256), random(0,256), random(0,256),BACK );
+  }
+  delay(5000);
+
+
+  Serial.println("Luminous を再起動");
+  resetLuminous();
+  Serial.print("再起動待ち");
+  while( !IsWakeUpLUMI() ) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Luminous 起動確認");  
+
+}
+// Luminous 起動確認関数
+boolean  IsWakeUpLUMI() {
+  boolean flag;
+  unsigned long cnt;
+
+  cnt=0;
+  flag=false;
+  Wire.requestFrom(LUM_ADDRESS,1);
+  while(Wire.available())  {
+    byte c = Wire.read();
+    if( c== 255 ) {
+      flag=true;
+      break;
+    }
+    cnt++;
+    if( cnt==0 ) { // cnt is overflow
+      flag=false;
+      break;
+    }
+  }  
+  return( flag );
+}
+//フルカラーLED １つに設定
+void  setFCLmode(  int no , int ledMode , int color1 , int color2 ) {
+  Wire.beginTransmission(LUM_ADDRESS);
+  Wire.write(LUM_FCL_SET) ;
+  Wire.write((byte)(0xff & no )) ;
+  Wire.write((byte)(0xff & ledMode)) ;
+  Wire.write((byte)(0xff & color1)) ;
+  Wire.write((byte)(0xff & color2)) ;
+  Wire.endTransmission();    delay(WAIT);
+}
+//フルカラーLED 全てに同一の設定
+void  setAllFCLmode( int ledMode , int color1 , int color2 ) {
+  Wire.beginTransmission(LUM_ADDRESS);
+  Wire.write(LUM_FCL_ALL_SET) ;
+  Wire.write((byte)(0xff & ledMode)) ;
+  Wire.write((byte)(0xff & color1)) ;
+  Wire.write((byte)(0xff & color2)) ;
+  Wire.endTransmission();    delay(WAIT);
+}
+//フルカラーLED 全ての明るさを設定
+void  setFCLbright(int updw ) {
+  if( updw==1 || updw==2 ) {
+    Wire.beginTransmission(LUM_ADDRESS);
+    Wire.write(LUM_FCL_BRIGHT) ;
+    Wire.write((byte)(0xff & updw)) ;
+    Wire.endTransmission();    delay(WAIT);
+  }
+}
+//
+void  setFCLcolor( int no , int r , int g,int b , int fb ) {
+    Wire.beginTransmission(LUM_ADDRESS);
+    Wire.write(LUM_FCE_COLOR_SET) ;
+    Wire.write((byte)(0xff & no )) ;
+    Wire.write((byte)(0xff & r)) ;
+    Wire.write((byte)(0xff & g)) ;
+    Wire.write((byte)(0xff & b)) ;
+    Wire.write((byte)(0xff & fb)) ;
+    Wire.endTransmission();    delay(WAIT);
+}
+
+//モノカラーLED 全てを同一設定
+void  sendMCLmodeAll() {
+  Wire.beginTransmission(LUM_ADDRESS);
+  for(int i=0;i<(LED_MONO_COLOR_NUM+1);i++ ) {
+    Wire.write(i2cBuffer[i]) ;
+  }
+  Wire.endTransmission();    delay(WAIT);
+}
+//モノカラーLED １つを設定
+void  setMCLmode( int no , int ledMode) {
+  Wire.beginTransmission(LUM_ADDRESS);
+  Wire.write(LUM_MCL_SET);
+  Wire.write((byte)(0xff & no ));
+  Wire.write((byte)(0xff & ledMode));
+  Wire.endTransmission();    delay(WAIT);
+
+}
+//Luminousを再起動
+void  resetLuminous() {
+  Wire.beginTransmission(LUM_ADDRESS);
+  Wire.write(LUM_INIT);
+  Wire.endTransmission();  
+}
+
---
+++ LuminousCommon.h
@@ -0,0 +1,190 @@
+//******************* LED Blink mode *************
+// LEDの点灯モード
+//LED消灯
+#define   LED_OFF           0x00
+//LED点灯
+#define   LED_ON            0x10
+//LED点滅
+#define   LED_BLINK         0x20
+//LED 長く消灯１回点灯
+#define   LED_BLINK_OFFON   0x30
+//LED 長く消灯２回点灯
+#define   LED_BLINK_OFFONON 0x40
+//LED だんだん明るくだんだん暗く
+#define   LED_BLINK_STUD    0x50
+//LED 急に明るく急に暗く
+#define   LED_BLINK_ST2UD   0x60
+//LED だんだん明るく消灯
+#define   LED_BLINK_STU     0x70
+
+//点灯モードは上位4ビット
+#define MODE_MASK   0xf0
+
+//LEDの点滅スピード
+#define LED_SPEED_LEVEL0  0 
+#define LED_SPEED_LEVEL1  1
+#define LED_SPEED_LEVEL2  2
+#define LED_SPEED_LEVEL3  3
+#define LED_SPEED_LEVEL4  4
+#define LED_SPEED_LEVEL5  5
+#define LED_SPEED_LEVEL6  6
+#define LED_SPEED_LEVEL7  7
+#define LED_SPEED_LEVEL8  8 
+#define LED_SPEED_LEVEL9  9
+#define LED_SPEED_LEVEL10 10
+#define LED_SPEED_LEVEL11 11
+#define LED_SPEED_LEVEL12 12
+#define LED_SPEED_LEVEL13 13
+#define LED_SPEED_LEVEL14 14
+#define LED_SPEED_LEVEL15 15
+
+#define LED_SPEED_SLOW    0
+#define LED_SPEED_NORMAL  4
+#define LED_SPEED_FAST    5
+#define LED_SPEED_VFAST   7
+
+#define LED_SPEED_ANALOG_SLOW 12
+#define LED_SPEED_ANALOG_NORMAL 14
+#define LED_SPEED_ANALOG_FAST 15
+
+//LED点滅等の間隔設定は下位4ビット
+#define SPEED_MASK   0x0f
+
+// LEDの点滅スピード用のタイミング（ｍSec)  
+#define LED_BLINK_WAIT0   2000
+#define LED_BLINK_WAIT1   1600
+#define LED_BLINK_WAIT2   1200
+#define LED_BLINK_WAIT3   1000
+#define LED_BLINK_WAIT4   800
+#define LED_BLINK_WAIT5   600
+#define LED_BLINK_WAIT6   400
+#define LED_BLINK_WAIT7   200
+#define LED_BLINK_WAIT8   8
+#define LED_BLINK_WAIT9   7
+#define LED_BLINK_WAIT10  6
+#define LED_BLINK_WAIT11  5
+
+#define LED_BLINK_WAIT12  4
+
+#define LED_BLINK_WAIT13  2
+#define LED_BLINK_WAIT14  1
+#define LED_BLINK_WAIT15  0
+
+// I2C関係
+// 本機のSLAVEアドレス
+#define LUM_ADDRESS         0x11
+//フルカラーLED１つに点滅モード・カラーを設定
+#define LUM_FCL_SET         0x01
+//フルカラーLEDの全体の明るさ設定
+#define LUM_FCL_BRIGHT      0x02
+//フルカラーLEDに個別にRGBカラーを設定
+#define LUM_FCE_COLOR_SET   0x04
+//フルカラーLEDすべてに同じ点滅モード・カラーを設定
+#define LUM_FCL_ALL_SET     0x11
+//モノカラーLED１つに点灯モードを設定
+#define LUM_MCL_SET         0x21
+//モノカラーLEDすべてに同じ点滅モード・カラーを設定
+#define LUM_MCL_ALL_SET     0x31
+//本機を初期化
+#define LUM_INIT            0x7f
+
+// モノカラーLEDの数
+#define LED_MONO_COLOR_NUM  8
+
+// フルカラーLEDの数
+#define LED_FULL_COLOR_NUM 24
+// フルカラーLEDの制御用のPIN
+#define LED_FULL_COLOR_CONT_PIN 2
+
+//======================================== LED COLOR
+#define LED_RED       0
+#define LED_BLUE      1
+#define LED_GREEN     2
+#define LED_ORANGE    3
+#define LED_PINK      4
+#define LED_YELLOW    5
+#define LED_WHITE     6
+#define LED_SAKURA    9
+#define LED_CYAN      7
+#define LED_BLACK     8
+#define LED_LIGHTGREEN  10
+#define LED_LEMON     11
+#define LED_VIOLET    12
+#define LED_PURPLE    13
+
+//========================================== LED FRONT or BACK
+#define FRONT 1
+#define BACK  2
+
+#ifndef LED_DEF
+#define LED_DEF 1
+
+// LED点滅速度設定用
+int   ledBlinkDiff[]={
+  LED_BLINK_WAIT0,LED_BLINK_WAIT1,LED_BLINK_WAIT2,LED_BLINK_WAIT3,
+  LED_BLINK_WAIT4,LED_BLINK_WAIT5,LED_BLINK_WAIT6,LED_BLINK_WAIT7,
+  LED_BLINK_WAIT8,LED_BLINK_WAIT9,LED_BLINK_WAIT10,LED_BLINK_WAIT11,
+  LED_BLINK_WAIT12,LED_BLINK_WAIT13,LED_BLINK_WAIT14,LED_BLINK_WAIT15 };
+// 長く消灯、１回点灯時の制御用配列関係
+byte  BlinkOFFON[]={0, 0, 0, 0, 0, 0, 1};
+#define BlinkOFFON_NUM   7
+// 長く消灯、２回点灯時の制御用配列関係
+byte  BlinkOFFONON[]={0, 0, 0, 0, 0, 0, 1, 0, 1};
+#define BlinkOFFONON_NUM   9
+
+//-----------------------------------For Mono Color LEDs
+struct  LEDARRAY_MONO_COLOR {
+  byte           ModeNo,Count;
+  unsigned  long  LastUpDateTime;
+};
+class LED_CONTROL_MONO_COLOR {
+public:
+  struct  LEDARRAY_MONO_COLOR LEDMC[LED_MONO_COLOR_NUM];
+
+  void  defaultSetting(void);
+  void  showLED(int);  //LED No
+  void  showLEDall(void);
+  void  LEDOff(int);  //LED No
+  void  LEDOn(int);  //LED No
+  void  setLED(int,byte);   //LED No,Mode( STYLE | SPEED
+  void  LEDBlink(int);   //LED No
+  void  blinkSTUD(int);  // Step UP->DOWN
+  void  blinkST2UD(int); // Step UP2 -> DOWN2
+  void  blinkOffOn(int);  //LED No
+  void  blinkOffOnOn(int);  //LED No
+  void  blinkSTU( int );  //LED No
+};
+
+//========================================For Full Color  LEDs
+struct  LEDARRAY_FULL_COLOR {
+  byte          ModeNo,Count;
+  unsigned long LastUpDateTime;
+  unsigned int  r,g,b,rb,gb,bb; // FRONT RGB &  BACK RGB
+};
+class LED_CONTROL_FULL_COLOR {
+public:
+  struct  LEDARRAY_FULL_COLOR LEDFC[LED_FULL_COLOR_NUM];
+  int   LEDBright;
+
+  void  defaultsetting(void);
+  void  showLEDall(void);
+  void  initLED(int );
+  void  BrightUpDown(int);
+  void  LEDOff(int);
+  void  LEDOn(int);
+  void  LEDBlink(int);
+  void  BlinkOffOn( int ) ;
+  void  BlinkOffOnOn( int  ) ;
+  void  blinkSTUD( int );
+  void  blinkST2UD(int);
+  void  blinkSTU(int);
+  void  setLEDColorMode( int, int ,int  ,int );
+  void  setLEDMode(int,int);
+  void  setLEDColor( int , int ,int ) ;
+  void  setLEDColorFB( int , int  , int );
+  void  SetTraficSignalColor( int , int  );
+  void  SetTraficSignalColor2( int , int  );
+  void  SetDiscomffortIndex(int , int);
+};
+
+#endif
